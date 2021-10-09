0ef11e4e8194c2b5f9e0f97367f6470deedff823
PashkovDanilASU <properina45@gmail.com>
2021-10-09 21:45:25 +0300

    Alpha 2
    Выполнил проверку ввода для числовых данных(для трубы и КС). Проработал навигацию по меню(switch).

--- PashkovDanilPoject/PashkovDanilPoject.cpp
+++ PashkovDanilPoject/PashkovDanilPoject.cpp
@@ -23,22 +23,66 @@ struct CS
     double performance;
 };
 
-//PIPE
+// INPUT VALIDATION
+void number_input_validation(bool& check)
+{
+    if (cin.peek() != '\n' || !cin) 
+    {
+        cout << "You entered an invalid number, enter a new number: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+        check = false;
+    }
+    else 
+    {
+        check = true;
+    }
+};
+/**/
+
+
+// PIPE
 void length_pipe(double& length)
 {
-    cout << "Enter pipe length in meters:";
-    cin >> length;
+    cout << "Enter pipe length in meters (10 - 20):";
+    bool check;
+    while (true)
+    {
+        cin >> length;
+        number_input_validation(check);
+        if (check && (length > 20 || length < 10))
+        {
+            cout << "You entered an invalid number, enter a new number: ";
+        }
+        else if (check)
+        {
+            break;
+        }
+    };
 };
 
 void diametr_pipe(int& diametr)
 {
-    cout << "Enter pipe diametrs in millimeters:";
-    cin >> diametr;
+    cout << "Enter pipe diametrs in millimeters (630 - 1420):";
+    bool check;
+    while (true)
+    {
+        cin >> diametr;
+        number_input_validation(check);
+        if (check && (diametr > 1420 || diametr < 630))
+        {
+            cout << "You entered an invalid number, enter a new number: ";
+        }
+        else if (check)
+        {
+            break;
+        }
+    };  
 };
 
 void ready_unready_pipe(bool& repair)
 {
-    cout << "in Repair or not in repair? Push on your keyboard 'y' or 'n'. ";
+    cout << "in Rrepair or not in repair? Push on your keyboard 'y' or 'n'. ";
     char flagpipe;
     while (true)
     {
@@ -46,19 +90,23 @@ void ready_unready_pipe(bool& repair)
         if (flagpipe == 'y') 
         {
             repair = true;
+            cin.clear();
+            cin.ignore(10000, '\n');
             break;
         }
         else if (flagpipe == 'n') 
         {
             repair = false;
+            cin.clear();
+            cin.ignore(10000, '\n');
             break;
         }
     }
     
 };
-//PIPE
+/**/
 
-//CS
+// CS
 void CS_name(string& name)
 {
     cout << "Enter CS name:";
@@ -67,27 +115,71 @@ void CS_name(string& name)
 
 void CS_count(int& count)
 {
-    cout << "Enter CS count:";
-    cin >> count;
+    cout << "Enter CS count (1-15):";
+    bool check;
+    while (true)
+    {
+        cin >> count;
+        number_input_validation(check);
+        if (check && (count > 15 || count < 1))
+        {
+            cout << "You entered an invalid number, enter a new number: ";
+        }
+        else if (check)
+        {
+            break;
+        }
+
+    };
+ 
 };
 
-void CS_count_ready(int& count_ready)
+void CS_count_ready(int& count, int& count_ready)
 {
     cout << "Enter CS which ready to work, in range (0 to n), where n your CS.";
-    cin >> count_ready;
+    bool check;
+    while (true)
+    {
+        cin >> count_ready;
+        number_input_validation(check);
+        if (check && (count < count_ready || count_ready < 0))
+        {
+            cout << "You entered an invalid number, enter a new number: ";
+        }
+        else if (check)
+        {
+            break;
+        }
+
+    };
+    
 };
 
 void CS_performance(double& performance)
 {
     cout << "Enter the performance of CS, in range(0 to 100%).";
-    cin >> performance;
+    bool check;
+    while (true)
+    {
+        cin >> performance;
+        number_input_validation(check);
+        if (check && (performance > 100 || performance < 0))
+        {
+            cout << "You entered an invalid number, enter a new number: ";
+        }
+        else if (check)
+        {
+            break;
+        }
+    };
+    
 };
-//CS
+/**/
 
-//CONSOLE
+// CONSOLE
 void clear_console_space()
 {
-    cout << string(30, '\n');
+    system("cls");
 };
 
 void menu_display()
@@ -97,12 +189,17 @@ void menu_display()
         "3. View all objects\n"
         "4. Edit pipe\n"
         "5. Edit CS\n"
-        "6. Edit pipe\n"
-        "7. Save\n"
-        "8. Load\n"
+        "6. Save\n"
+        "7. Load\n"
         "0. Exit\n";
 };
-//CONSOLE
+
+void back_to_menu()
+{
+    clear_console_space();
+    menu_display();
+}
+/**/
 
 int main()
 {
@@ -110,13 +207,45 @@ int main()
     CS new_CS;
     new_pipe.id = 1;
     new_CS.id = 1;
-
+    int menu_navigator;
     menu_display();
-    CS_name(new_CS.name);
-    length_pipe(new_pipe.length);
-    diametr_pipe(new_pipe.diametr);
-    return 0;
-}
+    while (true)
+    {
+        menu_navigator = getchar();
+        cin.clear();
+        cin.ignore(10000, '\n');
+        switch (menu_navigator)
+        {
+        case '1':
+            clear_console_space();
+            length_pipe(new_pipe.length);
+            diametr_pipe(new_pipe.diametr);
+            ready_unready_pipe(new_pipe.ready_unready);
+            back_to_menu();
+        break;
+        case '2':
+            clear_console_space();
+            CS_name(new_CS.name);
+            CS_count(new_CS.count);
+            CS_count_ready(new_CS.count, new_CS.count_ready);
+            CS_performance(new_CS.performance);
+            back_to_menu();
+            break;
+        case '3':
+            clear_console_space();
+            cout << "Pipe characteristics." << endl << "Length pipe: " << new_pipe.length << endl << "Diamtre pipe: " << new_pipe.diametr << endl << "Pipe readiness: " << new_pipe.ready_unready << endl
+            << endl << "CS characteristics." << endl << "CS name:" << new_CS.name << endl << "CS count:" << new_CS.count << endl << "count of serviceable CS: " << new_CS.count_ready << endl << "CS performance: " << new_CS.performance;
+            back_to_menu();
+        break;
+        case '0': 
+            clear_console_space();
+            return 0;
+            break;
+        default:
+            break;
+        };
+    };
+};
 
 
 
